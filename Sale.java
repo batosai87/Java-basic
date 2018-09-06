0c76027c114ce4150aedeca68357bf8de67349d6
atma <atma@gmail.com>
2018-09-06 14:44:55 +0700

    revision value secondfix

--- Sale.java
+++ Sale.java
@@ -6,8 +6,8 @@ public class Sale {
 		double price, tax, total;
 		
 		price = 30.79;
-		tax = 1.90;
-		total = 31.30;
+		tax = 2.90;
+		total = 345.30;
 		System.out.println("The price of the item " + "is " + price);
 		System.out.println("The tax is " + tax);
 		System.out.println("The total is " + total);
